4d2af7a398af37c3d44236d4f0f5ebc70cb8c2ff
PedroUnipam <130712531+PedroUnipam@users.noreply.github.com>
2026-03-18 00:35:42 +0000

    tabs navigation

--- aula05-modal/exModal/App.js
+++ aula05-modal/exModal/App.js
@@ -1,11 +1,36 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import CustomModalScreen from "./components/customModal";
+import { NavigationContainer, TabActions } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return(
     <SafeAreaProvider>
-      <CustomModalScreen animation="slide" themeColor="#2196F3"/>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: "#000",
+            tabBarLabelStyle:{ fontSize: 13, fontWeight: "bold",
+            }
+          
+          }}
+        >
+          <Tab.Screen name="slide">
+            {() => <CustomModalScreen animation="slide" themeColor="#2196F3"/>}
+          </Tab.Screen>
+          
+          <Tab.Screen name="fade">
+            {() => <CustomModalScreen animation="fade" themeColor="#4caf50"/>}
+          </Tab.Screen>
+          
+          <Tab.Screen name="none">
+            {() => <CustomModalScreen animation="none" themeColor="#ff9800"/>}
+          </Tab.Screen>
+        </Tab.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
