0ce073c149b0713f2eb8db5111921be04cae626f
CawaEstevan <145603770+CawaEstevan@users.noreply.github.com>
2026-04-15 03:20:58 +0000

    ok

--- atividade/ex01/screens/HomeScreen.js
+++ atividade/ex01/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native'; 
 import { HomeStyles } from '../styles/HomeStyles';
 
 export default function HomeScreen() {
@@ -9,12 +9,19 @@ export default function HomeScreen() {
   return (
     <View style={HomeStyles.container}>
       <Text style={HomeStyles.titulo}>Bem-vindo!</Text>
+      
       <Text style={HomeStyles.mensagem}>
-        Utilize o menu de navegação lateral para acessar os modais e scrolls.
+        Utilize o menu de navegação lateral para acessar os modais.
       </Text>
-      <TouchableOpacity
-        style={{ marginTop: 20, padding: 15, backgroundColor: '#4A90E2', borderRadius: 8 }}
-        onPress={() => navigation.openDrawer()}
+
+      <TouchableOpacity 
+        style={{ 
+          marginTop: 20, 
+          padding: 15, 
+          backgroundColor: '#4A90E2', 
+          borderRadius: 8 
+        }}
+        onPress={() => navigation.openDrawer()} // Abre a gaveta
       >
         <Text style={{ color: '#fff', fontWeight: 'bold' }}>ABRIR MENU</Text>
       </TouchableOpacity>
